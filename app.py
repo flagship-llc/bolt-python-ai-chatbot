d6224b83ba4658fedd5ccfa7e42a6303529cbde9
djjimba <koji@flagship.cc>
2025-02-16 13:15:47 +0900

    Introducing Flask
    SocketModeHandler was much cooler though..

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import os
 import logging
-
+from flask import Flask
 from slack_bolt import App
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-
+from slack_bolt.adapter.flask import SlackRequestHandler
 from listeners import register_listeners
 
-# Initialization
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+# Initialize Flask and Slack Bolt apps
+flask_app = Flask(__name__)
+slack_app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+handler = SlackRequestHandler(slack_app)
+
+# Configure logging
 logging.basicConfig(level=logging.DEBUG)
 
-# Register Listeners
-register_listeners(app)
+# Register Slack Listeners
+register_listeners(slack_app)
 
-# Start Bolt app
+# Create Flask route for Slack events
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    return handler.handle(flask_app.request)
+
+# Start Flask app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
+    flask_app.run(port=3000)
